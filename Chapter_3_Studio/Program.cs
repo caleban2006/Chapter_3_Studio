13547bd27f0e767bcf3353bc2eab13b2693e17eb
caleban2006 <logan.jen.2005@gmail.com>
2020-06-03 13:37:26 -0400

    some bonus missions completed

--- Chapter_3_Studio/Program.cs
+++ Chapter_3_Studio/Program.cs
@@ -6,7 +6,27 @@ namespace Chapter_3_Studio
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //string myString = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc accumsan sem ut ligula scelerisque sollicitudin. Ut at sagittis augue. Praesent quis rhoncus justo. Aliquam erat volutpat. Donec sit amet suscipit metus, non lobortis massa. Vestibulum augue ex, dapibus ac suscipit vel, volutpat eget massa. Donec nec velit non ligula efficitur luctus.";
+
+            Console.WriteLine("Enter a sentence:");
+            string myString = Console.ReadLine().ToLower();
+
+            //char[] charactersInString = myString.ToCharArray();
+
+            while (myString.Length > 0)
+            {
+                Console.Write(myString[0] + " : ");
+                int count = 0;
+                for (int i = 0; i < myString.Length; i++)
+                {
+                    if (myString[0] == myString[i])
+                    {
+                        count++;
+                    }
+                }
+                Console.WriteLine(count);
+                myString = myString.Replace(myString[0].ToString(), string.Empty);
+            }
         }
     }
 }
